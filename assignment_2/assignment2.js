3394fb83527c94cf3c2535ef4efa4dcd970badf6
RubyLoopy <baylisadeschool@gmail.com>
2017-07-29 12:32:40 -0400

    Done July 29 12:30pm

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,9 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for(i = 0; i <101; i++){
+    console.log(i);
+}
 
 
 /*
@@ -17,3 +20,10 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var hashtag = '';
+
+for (var i = o; i <7; i ++){
+    hashtag = hashtag + '#';
+    console.log(hashtag);
+}
